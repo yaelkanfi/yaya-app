ec198703294547afe6b213f8086e0e18cce1194c
Yael.Ben_David <ykanfi@g.jct.ac.il>
2024-10-30 11:03:36 +0200

    header ui

--- home-decor-website/front/src/components/header/Header.js
+++ home-decor-website/front/src/components/header/Header.js
@@ -1,26 +1,49 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { FaShoppingCart } from 'react-icons/fa';
+import { FaShoppingCart, FaSearch, FaChair, FaBed, FaLightbulb } from 'react-icons/fa';
 import { CartContext } from '../../CartContext';
 import './Header.css';
 
 const Header = () => {
-
-  const { getCartCount} = useContext(CartContext);
+  const { getCartCount } = useContext(CartContext);
   const cartCount = getCartCount();
+
+  const categories = [
+    { name: 'Furniture', icon: <FaChair />, subcategories: ['Sofas', 'Chairs', 'Tables'] },
+    { name: 'Bedding', icon: <FaBed />, subcategories: ['Mattresses', 'Pillows', 'Blankets'] },
+    { name: 'Lighting', icon: <FaLightbulb />, subcategories: ['Ceiling', 'Table Lamps', 'Outdoor'] },
+  ];
 
   return (
     <header className="header">
-      <h1>Home Decor</h1>
-      <nav>
-        <Link to="/">Home</Link> {/* Use Link instead of a */}
-        <Link to="/about">About</Link>
-        <Link to="/products">Products</Link>
+      <div className="header-top">
+        <Link to="/" className="header-title no-link-style">KBN</Link>
+
+        <div className="search-bar">
+          <input type="text" placeholder="I'M LOOKING FOR..." />
+          <button><FaSearch /></button>
+        </div>
+
+        {/* Cart Icon */}
         <Link to="/cart" className="cart-icon-container">
           <FaShoppingCart className="cart-icon" size={24} />
           {cartCount > 0 && <span className="cart-count">{cartCount}</span>}
         </Link>
-        {/* Add more navigation links as needed */}
+      </div>
+
+      {/* Categories Positioned Below Search Bar */}
+      <nav className="category-nav">
+        {categories.map((category, index) => (
+          <div key={index} className="category-item">
+            <span className="category-icon">{category.icon}</span>
+            <span className="category-name">{category.name}</span>
+            <div className="subcategory-dropdown">
+              {category.subcategories.map((sub, idx) => (
+                <Link key={idx} to={`/products/${category.name.toLowerCase()}/${sub.toLowerCase()}`}>{sub}</Link>
+              ))}
+            </div>
+          </div>
+        ))}
       </nav>
     </header>
   );
